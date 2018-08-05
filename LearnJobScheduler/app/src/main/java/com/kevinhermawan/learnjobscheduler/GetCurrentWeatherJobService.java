8fff6308f1ceed6e1d356351dd6b4ba7893be1ba
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-08-05 22:36:53 +0700

    Learn Job Scheduler

---
+++ LearnJobScheduler/app/src/main/java/com/kevinhermawan/learnjobscheduler/GetCurrentWeatherJobService.java
@@ -0,0 +1,91 @@
+package com.kevinhermawan.learnjobscheduler;
+
+import android.app.NotificationManager;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.Header;
+
+public class GetCurrentWeatherJobService extends JobService {
+
+    public static final String TAG = GetCurrentWeatherJobService.class.getSimpleName();
+
+    final String APP_ID = "a72beda3fa6c94a9e1efb49f7cedb100";
+    final String CITY = "Jakarta";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "onStartJob() Executed");
+        getCurrentWeather(jobParameters);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.d(TAG, "onStopJob() Executed");
+        return true;
+    }
+
+    private void getCurrentWeather(final JobParameters job) {
+        Log.d(TAG, "Running");
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+CITY+"&appid="+APP_ID;
+        Log.e(TAG, "getCurrentWeather: "+url );
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject responseObject = new JSONObject(result);
+                    String currentWeather = responseObject.getJSONArray("weather").getJSONObject(0).getString("main");
+                    String description = responseObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                    double tempInKelvin = responseObject.getJSONObject("main").getDouble("temp");
+                    double tempInCelcius = tempInKelvin - 273;
+                    String temperature = new DecimalFormat("##.##").format(tempInCelcius);
+                    String title = "Current Weather";
+                    String message = currentWeather+", "+description+" with "+temperature+" celcius";
+                    int notifId = 100;
+                    showNotification(getApplicationContext(), title, message, notifId);
+                    jobFinished(job, false);
+                }catch(Exception e) {
+                    jobFinished(job, true);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                jobFinished(job, true);
+            }
+        });
+    }
+
+    private void showNotification(Context context, String title, String message, int notifId) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.black))
+                .setVibrate(new long[] {1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+
+        mNotificationManager.notify(notifId, builder.build());
+    }
+}
